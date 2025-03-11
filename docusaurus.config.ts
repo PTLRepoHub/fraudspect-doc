6e663ea6be5a3517c0f9e799503ce1d592e85383
Sylvernus Akubo <akubosylvernus@gmail.com>
2025-03-11 19:43:02 +0100

    updae

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,12 +10,11 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://doc.getfraudspect.com",
+  url: "https://doc.getfraudspect.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/fraudspect-doc/",
+  baseUrl: "/",
 
-  deploymentBranch: "gh-pages",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "PTLRepoHub", // Usually your GitHub org/user name.
